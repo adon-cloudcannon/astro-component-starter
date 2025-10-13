794241b7ba068bce8ddf50b36f3da8209936c546
Mike Neumegen <mike@cloudcannon.com>
2025-10-13 23:22:26 +0000

    Updated 1 file via CloudCannon.

--- live-editing.js
+++ live-editing.js
@@ -5,5 +5,7 @@ import "@cloudcannon/editable-regions/astro-react-renderer";
 import { registerAstroComponent } from "@cloudcannon/editable-regions/astro";
 // @ts-ignore
 import Heading from "./src/components/typography/heading/heading.astro";
+import SimpleText from "./src/components/typography/simple-text/simple-text.astro";
 
 registerAstroComponent("typography/heading", Heading);
+registerAstroComponent("typography/simple-text", SimpleText);
